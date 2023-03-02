2c2303e02d666362351e964da0924047ae283ee1
ritikaSolulab <126669605+ritikaSolulab@users.noreply.github.com>
2023-03-02 11:45:28 +0530

    Delete routes directory

--- routes/routes.js
+++
@@ -1,7 +0,0 @@
-const router = require("express").Router();
-const stockApi = require("../controllers/stock");
-
-//get the stock api
-router.get('/googleStockPrice/:symbol', stockApi);
-
-module.exports = router;
